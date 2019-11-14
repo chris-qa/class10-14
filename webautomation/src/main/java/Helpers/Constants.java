bc1f4b3f830aa511933a63f82041dd7a4e7c19fd
chris-qa <cchiu@wheelsup.com>
2019-11-14 10:01:16 -0500

    removed bad

--- webautomation/src/main/java/Helpers/Constants.java
+++
@@ -1,7 +0,0 @@
-package Helpers;
-
-public class Constants {
-    public static String MARKETING_BASE_PATH = "https://wheelsup.com/";
-
-    public static String CHARTER_MARKET_PLACE = "/charter-marketplace";
-}
